1e14cf3e2bd17d48d78b9dd479b298b9730ff579
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-17 17:43:20 +0300

    void

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -2,7 +2,7 @@ import IngredientsList from "../IngredientList/ingredientList";
 import Instructions from "../Instructions/Instructions";
 
 const Recipe = ({ name, ingredients, steps }) => (
-  <section id={name.toLoverCase().replace(/ /g, "-")}>
+  <section id={name.toLowerCase().replace(/ /g, "-")}>
     <h1>{name}</h1>
     <IngredientsList list={ingredients} />
     <Instructions title="Cooking Instructions" steps={steps} />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import Menu from "./components/Menu/Menu";
-import data from "./data/recipes";
+import data from "../data/recipes";
 
 window.React = React;
+
 render(<Menu recipes={data} />, document.getElementById("react-container"));
