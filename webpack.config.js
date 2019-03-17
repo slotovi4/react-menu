0d56cfbb5051f654cba3fceb0f2a3416de69bf16
Роман Брунс <romanbruns@MacBook-Air-Roman.local>
2019-03-17 17:44:21 +0300

    void

--- webpack.config.js
+++ webpack.config.js
@@ -37,12 +37,6 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production")
-      }
-    }),
-
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       warnings: false,
